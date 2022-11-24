bef5404a0e1d5100edbe225faf1c2f3c2d8e7e92
Raúl Gilabert Gámez <raulgilabert@proton.me>
2022-11-24 12:09:34 +0100

    maravilloso y magnifico generador de vectores

--- maravilloso_y_magnifico_generador_de_vectores.cc
+++ maravilloso_y_magnifico_generador_de_vectores.cc
@@ -4,23 +4,34 @@
 
 using namespace std;
 
-#define SIZE 100000
+#define SIZE 10
 
-int main() {
-    int a[SIZE];
+int main(int argc, char *argv[]) {
+    int quantity = 1;
+    int elements = SIZE;
+
+
+   if (argc > 1)
+        quantity = atoi(argv[1]);
+        
+    if (argc > 2)
+        elements = atoi(argv[2]);
+
+    cout << elements << endl << endl;
     
-    for (int i = 0; i < SIZE; i++)
-	a[i] = i;
+    for (int i = 0; i < quantity; ++i) { 
+        int a[elements];
+        
+        for (int i = 0; i < elements; i++)
+            a[i] = i;
 
-    for (int i = 0; i < SIZE; i++)
-        swap(a[i], a[rand() % SIZE]);
-
-    cout << SIZE << endl << endl;
-
-    for (int i = 0; i < SIZE; ++i) {
-	cout << a[i] << ' ';
+        for (int i = 0; i < elements; i++)
+            swap(a[i], a[rand() % elements]);
+        for (int i = 0; i < elements; ++i) {
+            cout << a[i] << ' ';
+        }
+        cout << endl << endl << endl << endl << endl << endl << endl << endl;
     }
-    cout << endl;
 }
 
 
