342617e5d0772523ef2db5935328879a70027d7b
Raúl Gilabert Gámez <raulgilabert@proton.me>
2022-12-01 11:11:08 +0100

    ejecutador

--- ejecutador.sh
+++ ejecutador.sh
@@ -2,5 +2,7 @@
 
 for i in {0..10}
 do
-    python3 maravilloso_y_magnifico_generador_de_vectores.py > generado.txt && $1 < generado.txt
+    python3 maravilloso_y_magnifico_generador_de_vectores.py > generado.txt && $1 < generado.txt >> salida.txt
 done
+
+grep salida.txt Time
